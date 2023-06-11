0a7b66df79bb0af40ff065bbe6eed3a07d8b7dfd
Igor Lucas <81535198+IgorLucasAssuncao@users.noreply.github.com>
2023-06-11 16:10:24 -0300

    commit linkagem

--- detalhes.js
+++ detalhes.js
@@ -3,10 +3,10 @@ const productId = urlParams.get("id");
 
 // Requisição de detalhes usando o ID do produto
 fetch(`https://diwserver.vps.webdock.cloud/products/${productId}`)
-    .then((res) => res.json())
-    .then((element) => {
-        let article = document.getElementById("DivProdutos");
-        article.innerHTML += `
+  .then((res) => res.json())
+  .then((element) => {
+    let article = document.getElementById("DivProdutos");
+    article.innerHTML += `
         <div class="produtos" id="${element.id}">
           <img src="${element.image}" alt="">
           <h5><a href="./detalhes.html?id=${element.id}">${element.title}</a></h5>
@@ -16,6 +16,4 @@ fetch(`https://diwserver.vps.webdock.cloud/products/${productId}`)
           <p>Descrição: ${element.description}</p>
           <a href="../detalhes/detalhes.html?id=${element.id}"><button id="detailsButton" class="card-button" >Comprar</button></a>
         </div>`;
-
-    });
- 
+  });
